1d7f4e9d4d8ca0e3d91cb4fb1ce110cf25e0513d
anooja-m <61877908+anooja-m@users.noreply.github.com>
2020-04-22 20:24:37 +0530

    first commit

---
+++ src/App.vue
@@ -0,0 +1,250 @@
+<template>
+<div>
+<!--Navbar-->
+<nav class="navbar navbar-expand-lg navbar-light white">
+
+  <div class="container"> 
+
+    <a class="navbar-brand" href="#">
+      <img src="./assets/icon.jpeg" height="30" alt="mdb logo">
+    </a>
+
+    <!-- Collapse button -->
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#basicExampleNav"
+            aria-controls="basicExampleNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <!-- Links -->
+    <div class="collapse navbar-collapse" id="basicExampleNav">
+
+      <!-- Left -->
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item">
+          <a class="nav-link waves-effect" href="https://mdbootstrap.com/docs/jquery/" target="_blank">World</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link waves-effect" href="https://mdbootstrap.com/docs/angular/" target="_blank">India</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link waves-effect" href="https://mdbootstrap.com/docs/react/" target="_blank">Business</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link waves-effect" href="https://mdbootstrap.com/docs/vue/" target="_blank">News</a>
+        </li>
+      </ul>
+
+      <!-- Right -->
+      <ul class="navbar-nav nav-flex-icons">
+        <li class="nav-item">
+          <a href="https://www.facebook.com/mdbootstrap" class="nav-link waves-effect" target="_blank">
+            <i class="fab fa-facebook-f"></i>
+          </a>
+        </li> 
+        <li class="nav-item">
+          <a href="https://twitter.com/MDBootstrap" class="nav-link waves-effect" target="_blank">
+            <i class="fab fa-twitter"></i>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a href="https://github.com/mdbootstrap/bootstrap-material-design" class="nav-link waves-effect"
+             target="_blank">
+            <i class="fab fa-github"></i>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a href="https://mdbootstrap.com/docs/jquery/newsletter/"
+             class="nav-link border border-light rounded waves-effect mr-2" target="_blank">
+            <i class="fas fa-envelope mr-1"></i>Newsletter
+          </a>
+        </li>
+      </ul>
+
+    </div>
+
+  </div>
+
+</nav>
+<!--/.Navbar-->
+  <div class="container my-5 py-5 z-depth-1">
+
+ 
+    <!--Section: Content-->
+    <section class="px-md-5 mx-md-5 dark-grey-text text-center text-lg-left">
+
+      <!--Grid row-->
+      <div class="row">
+
+        <!--Grid column-->
+        <div class="col-lg-6 mb-4 mb-lg-0 d-flex align-items-center justify-content-center">
+
+          <img src="./assets/covid.jpg" class="img-fluid" alt="">
+
+        </div>
+        <!--Grid column-->
+
+        <!--Grid column-->
+        <div class="col-lg-6 mb-4 mb-lg-0">
+
+          <h3 class="font-weight-bold">COVID-19 Latest Updates</h3>
+
+          <p class="font-weight-bold">That's a very nice subheading</p>
+
+          <p class="text-muted">Coronavirus world tracker: Total number of coronavirus cases in India has reached 18,985, and the global tally stands at 2,511,528. Stay with Fawzlabs News for Covid-19 corona LIVE updates</p>
+
+          <a class="font-weight-bold" href="#" >Learn more<i class="fas fa-angle-right ml-2"></i></a>
+
+        </div>
+        <!--Grid column-->
+
+      </div>
+      <!--Grid row-->
+
+
+    </section>
+    <!--Section: Content-->
+
+
+  </div>
+    <div class="container my-5 py-5 z-depth-1">
+ 
+
+    <!--Section: Content-->
+    <section class="text-center px-md-5 mx-md-5 dark-grey-text">
+
+      <!--Grid row-->
+      <div class="row d-flex justify-content-center">
+
+        <!--Grid column-->
+        <div class="col-lg-7">
+<div v-bind:key="index" v-for="(item,index) in coronaData">
+        </div>
+        </div>
+        <!--Grid column-->
+
+      </div>
+      <!--Grid row-->
+    </section>
+    <!--Section: Content-->
+  </div>
+  <div class="container mt-5">
+
+
+  <!--Section: Content-->
+  <section>
+
+  	<!-- Grid row -->
+    <div class="row">
+      
+      <!-- Grid column -->
+      <div class="col-lg-7 mb-4 align-self-center text-center text-lg-left">
+
+        <h2 class="h2 mb-5">Introducing an app for Fawzlabs News</h2>
+        <p class="text-muted mb-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit, error amet numquam iure provident voluptate esse quasi, veritatis totam voluptas nostrum quisquam eum porro a pariatur veniam. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit, error amet numquam iure provident voluptate esse quasi, veritatis totam voluptas nostrum.</p>
+
+        <button type="button" class="btn btn-cyan waves-effect btn-md ml-lg-0"><i class="fab fa-apple fa-lg pr-1"></i> App Store</button>
+				<button type="button" class="btn btn-cyan waves-effect btn-md"><i class="fab fa-google-play fa-lg pr-1"></i> Google Play</button>
+        
+      </div>
+      <!-- Grid column -->
+      
+      <!-- Grid column -->
+      <div class="col-lg-5 mb-4">
+
+				<!-- Featured image -->
+        <div class="view overlay">
+          <img class="img-fluid mx-auto" src="https://mdbootstrap.com/img/illustrations/app-user-colour.svg" alt="Sample image">
+          <a>
+            <div class="mask rgba-white-slight"></div>
+          </a>
+        </div>
+
+      </div>
+      <!-- Grid column -->
+
+    </div>
+    <!-- Grid row -->
+
+  </section>
+  <!--Section: Content-->
+
+
+</div>
+  <!-- Footer -->
+<footer class="page-footer font-small special-color-dark pt-4">
+ 
+  <!-- Footer Elements -->
+  <div class="container">
+
+    <!--Grid row-->
+    <div class="row">
+
+      <!--Grid column-->
+      <div class="col-md-6 mb-4">
+
+        <!-- Form -->
+        <form class="form-inline">
+          <input class="form-control form-control-sm mr-3 w-75" type="text" placeholder="Search"
+            aria-label="Search">
+          <i class="fas fa-search" aria-hidden="true"></i>
+        </form>
+        <!-- Form -->
+
+      </div>
+      <!--Grid column-->
+
+      <!--Grid column-->
+      <div class="col-md-6 mb-4">
+
+        <form class="input-group">
+          <input type="text" class="form-control form-control-sm" placeholder="Your email"
+            aria-label="Your email" aria-describedby="basic-addon2">
+          <div class="input-group-append">
+            <button class="btn btn-sm btn-outline-white my-0" type="button">Sign up</button>
+          </div>
+        </form>
+
+      </div>
+      <!--Grid column-->
+
+    </div>
+    <!--Grid row-->
+
+  </div>
+  <!-- Footer Elements -->
+
+  <!-- Copyright -->
+  <div class="footer-copyright text-center py-3">© 2020 Copyright:
+    <a href="https://mdbootstrap.com/education/bootstrap/"> Fawzlabs pvt.Ltd</a>
+  </div>
+  <!-- Copyright -->
+
+</footer>
+<!-- Footer -->
+</div>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+    coronaData:[]
+    }
+  },
+  mounted(){
+    this.getData()
+  }
+ ,methods:{getData(){
+            let config = {
+        headers: {
+          "x-rapidapi-host": "coronavirus-monitor.p.rapidapi.com",
+		"x-rapidapi-key": "J0Ro1CsdqumshmT7wG8r7qa4SIKjp1jqJHnjsntT9vFj2eaZzk"
+        }
+      }
+      axios.get('https://coronavirus-monitor.p.rapidapi.com/coronavirus/affected.php',config).then(response=>{
+          this.coronaData=response.data
+      })
+    },
+}
+};
+</script>
---
+++ src/views/india.vue
@@ -0,0 +1,113 @@
+
+<template>
+    
+<div class="container mt-5">
+  <!--Section: Content-->
+  <section class="dark-grey-text">
+
+    <!-- Section heading -->
+    <h2 class="text-center font-weight-bold mb-4 pb-2">COVID-19 INDIA</h2>
+    <!-- Section description -->
+    <p class="text-center mx-auto w-responsive mb-5">For any technical enquiry with respect to COVID19, you may kindly email on technicalquery.covid19@gov.in</p>
+
+  	<!-- Grid row -->
+    <div class="row align-items-center">
+
+      <!-- Grid column -->
+      <div class="col-lg-5 col-xl-4">
+
+        <!-- Featured image -->
+        <div class="view overlay rounded z-depth-1-half mb-lg-0 mb-4">
+          <img class="img-fluid" src="https://mdbootstrap.com/img/Photos/Others/images/49.jpg" alt="Sample image">
+          <a>
+            <div class="mask rgba-white-slight"></div>
+          </a>
+        </div>
+
+      </div>
+      <!-- Grid column -->
+
+      <!-- Grid column -->
+      <div class="col-lg-7 col-xl-8">
+
+        <!-- Post title -->
+        <h4 class="font-weight-bold mb-3"><strong>Title of the news</strong></h4>
+        <!-- Excerpt -->
+        <p class="dark-grey-text">Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit
+          quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus
+          et aut officiis cum soluta nobis est eligendi placeat facere aut rerum.</p>
+        <!-- Post data -->
+        <p>by <a class="font-weight-bold">Jessica Clark</a>, 19/04/2018</p>
+        <!-- Read more button -->
+        <a class="btn btn-primary btn-md mx-0 btn-rounded">Read more</a>
+
+      </div>
+      <!-- Grid column -->
+
+    </div>
+    <!-- Grid row -->
+
+    <hr class="my-5">
+
+  </section>
+  <!--Section: Content-->
+<div class="table-responsive">
+  <table class="table">
+    <thead>
+      <tr>
+        <th scope="col">#</th>
+        <th scope="col">Heading</th>
+        <th scope="col">Heading</th>
+        <th scope="col">Heading</th>
+        <th scope="col">Heading</th>
+        <th scope="col">Heading</th>
+        <th scope="col">Heading</th>
+        <th scope="col">Heading</th>
+        <th scope="col">Heading</th>
+        <th scope="col">Heading</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <th scope="row">1</th>
+        <td>Cell</td>
+        <td>Cell</td>
+        <td>Cell</td>
+        <td>Cell</td>
+        <td>Cell</td>
+        <td>Cell</td>
+        <td>Cell</td>
+        <td>Cell</td>
+        <td>Cell</td>
+      </tr>
+      <tr>
+        <th scope="row">2</th>
+        <td>Cell</td>
+        <td>Cell</td>
+        <td>Cell</td>
+        <td>Cell</td>
+        <td>Cell</td>
+        <td>Cell</td>
+        <td>Cell</td>
+        <td>Cell</td>
+        <td>Cell</td>
+      </tr>
+      <tr>
+        <th scope="row">3</th>
+        <td>Cell</td>
+        <td>Cell</td>
+        <td>Cell</td>
+        <td>Cell</td>
+        <td>Cell</td>
+        <td>Cell</td>
+        <td>Cell</td>
+        <td>Cell</td>
+        <td>Cell</td>
+      </tr>
+    </tbody>
+  </table>
+</div>
+
+</div>
+
+</template>
